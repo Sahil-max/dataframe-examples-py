e33a5918115b3ccf040772f0e2a342d9346c3fd0
Sahil-max <ksahilreddy@gmail,com>
2022-12-14 00:50:24 -0500

    test

--- dataframe/curation/dsl/finance_data_analysis.py
+++ dataframe/curation/dsl/finance_data_analysis.py
@@ -88,4 +88,4 @@ if __name__ == '__main__':
                       .otherwise("Standard").alias("Tier"))\
                 .show(5, False)
 
-# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" dataframe/curation/dsl/finance_data_analysis.py
+# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" --executor-core 5 --executor-memory 2g dataframe/curation/dsl/finance_data_analysis.py
